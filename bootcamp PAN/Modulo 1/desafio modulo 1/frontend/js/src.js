c2a039e9a0bd6ba8eaf9507882f3e5bc23623f28
Weslley do Cati Ferreira <weslley.docati@gmail.com>
2021-10-02 04:20:48 -0300

    Desafio modulo 1 - Adicionado o nome da role dos Employees na tabela | Refatoração de código duplicado

--- bootcamp PAN/Modulo 1/desafio modulo 1/frontend/js/src.js
+++ bootcamp PAN/Modulo 1/desafio modulo 1/frontend/js/src.js
@@ -1,147 +1,131 @@
-
-const employeesUrl = 'http://localhost:3000/employees'
-const rolesUrl = 'http://localhost:3000/roles'
-const tbody = document.querySelector('tbody')
-const rolesFilter = document.querySelector('.roles-filter')
-const select = document.querySelector('select')
-const totalEmployees = document.querySelector('.total')
+const employeesUrl = "http://localhost:3000/employees";
+const rolesUrl = "http://localhost:3000/roles";
+const tbody = document.querySelector("tbody");
+const rolesFilter = document.querySelector(".roles-filter");
+const select = document.querySelector("select");
+const totalEmployees = document.querySelector(".total");
 const orderOptions = [
-    {value:'name-asc', text:'Name ascendent'},
-    {value:'name-desc', text:'Name descendent'},
-    {value:'salary-asc', text:'Salary ascendent'},
-    {value:'salary-desc', text:'Salary descendent'}
-]
+  { value: "name-asc", text: "Name ascendent" },
+  { value: "name-desc", text: "Name descendent" },
+  { value: "salary-asc", text: "Salary ascendent" },
+  { value: "salary-desc", text: "Salary descendent" },
+];
 
+let employees, roles;
+let selectedFilter = new Set();
 
-let employees, roles
-let selectedFilter = new Set
-
-
-async function fetchJson(url){
-     const r = await fetch(url)
-    return await r.json()
+async function fetchJson(url) {
+  const r = await fetch(url);
+  return await r.json();
 }
 
-async function fetchData(){
-    [employees,roles] = await Promise.all([fetchJson(employeesUrl), fetchJson(rolesUrl)])
-    employees.sort((a, b) => a.name < b.name ? 1 : -1)
-    renderTable(employees)
-    renderSortBy(employees)
-    renderFilter(roles)
-    
-    
+async function fetchData() {
+  [employees, roles] = await Promise.all([
+    fetchJson(employeesUrl),
+    fetchJson(rolesUrl),
+  ]);
+  employees.sort((a, b) => (a.name < b.name ? 1 : -1));
+  renderTable(employees, roles);
+  renderSortBy(employees);
+  renderFilter(roles);
 }
 
-fetchData()
+fetchData();
 
-                      
-
-
-
-
-
-
-function renderTable(data){
-    if (tbody.hasChildNodes){
-        while(tbody.firstChild){
-            tbody.removeChild(tbody.lastChild)
-        }
+function renderTable(data) {
+  if (tbody.hasChildNodes) {
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.lastChild);
     }
+  }
 
-    const optionValue = select.value
+  const optionValue = select.value;
 
-    if (selectedFilter.size === 0){
-        data.forEach(element => {
-            tbody.insertAdjacentHTML('afterbegin',`<tr data-role["${element.role_id}"]>
-                <td>${element.id}</td>
-                <td class="emp-name">${element.name}</td>
-                <td>${element.role_id}</td>
-                <td>${element.salary}</td>
-                </tr>`)
-                
-        })
-    } else {
-            
-        data.forEach(element => {
-            if (selectedFilter.has(element.role_id)){
-            tbody.insertAdjacentHTML('afterbegin',`<tr data-role["${element.role_id}"]>
-                <td>${element.id}</td>
-                <td class="emp-name">${element.name}</td>
-                <td>${element.role_id}</td>
-                <td>${element.salary}</td>
-                </tr>`)
-            }
-        })
-    
-    }
-    totalEmployees.textContent = tbody.childElementCount
-}
+  if (selectedFilter.size === 0) {
+    data.forEach((element) => {
+      const [roleObj] = roles.filter((role) => role.id === element.role_id);
 
-function renderFilter(data){
- 
-    data.forEach(element => {
-        const div = document.createElement('div')
-        const inputCheckbox = document.createElement('input')
-        const label = document.createElement('label')
-        inputCheckbox.type = 'checkbox'
-        inputCheckbox.id = element.id
-        label.setAttribute('for', element.id)
-        label.textContent = element.name
-        div.appendChild(inputCheckbox)
-        div.appendChild(label)
-        inputCheckbox.addEventListener('change', (e) => {
-            if (e.target.checked === true){
-                selectedFilter.add(element.id)
-            } else {
-                selectedFilter.delete(element.id)
-            }
-            renderTable(employees)
-        })
-        rolesFilter.appendChild(div)
-        
+      tbodyAdjacent(element, roleObj);
     });
+  } else {
+    data.forEach((element) => {
+      const [roleObj] = roles.filter((role) => role.id === element.role_id);
+      if (selectedFilter.has(element.role_id)) {
+        tbodyAdjacent(element, roleObj);
+      }
+    });
+  }
+  function tbodyAdjacent(element, roleObj) {
+    tbody.insertAdjacentHTML(
+      "afterbegin",
+      `<tr>
+                <td>${element.id}</td>
+                <td class="emp-name">${element.name}</td>
+                <td>${roleObj.name}</td>
+                <td>${element.salary}</td>
+                </tr>`
+    );
+  }
+
+  totalEmployees.textContent = tbody.childElementCount;
 }
 
+function renderFilter(data) {
+  data.forEach((element) => {
+    const div = document.createElement("div");
+    const inputCheckbox = document.createElement("input");
+    const label = document.createElement("label");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = element.id;
+    label.setAttribute("for", element.id);
+    label.textContent = element.name;
+    div.appendChild(inputCheckbox);
+    div.appendChild(label);
+    inputCheckbox.addEventListener("change", (e) => {
+      if (e.target.checked === true) {
+        selectedFilter.add(element.id);
+      } else {
+        selectedFilter.delete(element.id);
+      }
+      renderTable(employees);
+    });
+    rolesFilter.appendChild(div);
+  });
+}
 
 //select.addEventListener('onchage', sortBy(select.value, employees))
 
-function renderSortBy(employees){
-    let sortedEmployees
-    
-    orderOptions.forEach((obj) => {
-        const option = document.createElement('option')
-        option.value = obj.value
-        option.textContent = obj.text
-        select.appendChild(option)
-        select.addEventListener('change', e => sort(e, employees))
-    })
+function renderSortBy(employees) {
+  let sortedEmployees;
 
+  orderOptions.forEach((obj) => {
+    const option = document.createElement("option");
+    option.value = obj.value;
+    option.textContent = obj.text;
+    select.appendChild(option);
+    select.addEventListener("change", (e) => sort(e, employees));
+  });
 
+  function sort(e, employees) {
+    const option = e.target.value;
 
-
-
-
-
-function sort (e, employees) {
-    const option = e.target.value
-    
-    if (option === 'name-asc'){
-        sortedEmployees = employees.sort((a, b) => a.name < b.name ? 1 : -1)
-    } else if (option === 'name-desc'){
-        sortedEmployees = employees.sort((a, b) => a.name > b.name ? 1 : -1)
-    } else   if (option === 'salary-asc'){
-        sortedEmployees = employees.sort((a, b) => a.salary < b.salary ? 1 : -1)
-    } else if (option === 'salary-desc'){
-        sortedEmployees = employees.sort((a, b) => a.salary > b.salary ? 1 : -1)
+    if (option === "name-asc") {
+      sortedEmployees = employees.sort((a, b) => (a.name < b.name ? 1 : -1));
+    } else if (option === "name-desc") {
+      sortedEmployees = employees.sort((a, b) => (a.name > b.name ? 1 : -1));
+    } else if (option === "salary-asc") {
+      sortedEmployees = employees.sort((a, b) =>
+        a.salary < b.salary ? 1 : -1
+      );
+    } else if (option === "salary-desc") {
+      sortedEmployees = employees.sort((a, b) =>
+        a.salary > b.salary ? 1 : -1
+      );
     } else {
-        sortedEmployees = employees
+      sortedEmployees = employees;
     }
-    renderTable(sortedEmployees)
+    renderTable(sortedEmployees);
+  }
+
+  return sortedEmployees;
 }
-
-
-return sortedEmployees
-
-
-}
-
