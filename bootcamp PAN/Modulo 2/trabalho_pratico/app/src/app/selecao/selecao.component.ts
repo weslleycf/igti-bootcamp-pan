f74dcc49424e451f819b52dc098824ea22aa18c3
Weslley do Cati Ferreira <weslley.docati@gmail.com>
2021-10-04 02:11:01 -0300

    Modulo 2 - Primeiro commit - estrutura básica componente app-selecao

---
+++ bootcamp PAN/Modulo 2/trabalho_pratico/app/src/app/selecao/selecao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selecao',
+  templateUrl: './selecao.component.html',
+  styleUrls: ['./selecao.component.css']
+})
+export class SelecaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
