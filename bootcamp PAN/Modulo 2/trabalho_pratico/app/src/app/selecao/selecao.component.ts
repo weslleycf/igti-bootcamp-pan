55f09eae3907b919fd5e7fdc7e10d279bf1dc82a
Weslley do Cati Ferreira <weslley.docati@gmail.com>
2021-10-04 12:32:22 -0300

    Modulo 2 - conclusão do core do trabalho prático

--- bootcamp PAN/Modulo 2/trabalho_pratico/app/src/app/selecao/selecao.component.ts
+++ bootcamp PAN/Modulo 2/trabalho_pratico/app/src/app/selecao/selecao.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-selecao',
   templateUrl: './selecao.component.html',
   styleUrls: ['./selecao.component.css']
 })
-export class SelecaoComponent implements OnInit {
+export class SelecaoComponent {
 
-  constructor() { }
+@Input() titulo: string = ""
 
-  ngOnInit(): void {
-  }
+@Input() opcoes: string[] = []
+
+@Input() escolhaAte: number = 0
+
+selectedTotal: number = 0
+
+
+selectedTotalChange(e:any):void{
+    e.currentTarget.checked ? this.selectedTotal++ : this.selectedTotal--
+}
 
 }
